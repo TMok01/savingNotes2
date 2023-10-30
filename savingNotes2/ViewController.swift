4ff6f9c4f27f636c772d7ef298d8afe17e4fd383
Tyler Mok <tymok06@gmail.com>
2023-10-30 10:00:30 -0500

    Initial Commit

---
+++ savingNotes2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  savingNotes2
+//
+//  Created by TYLER MOK on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
